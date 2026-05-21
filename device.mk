0e16413d9bad32609a53ddf679c3ced301c02eda
jhchong94 <jianhuaichong@gmail.com>
2026-05-21 09:40:43 +0100

    umi: Ship BCR in ROM

--- device.mk
+++ device.mk
@@ -18,6 +18,9 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
 
+# BCR (Basic Call Recorder)
+$(call inherit-product, vendor/bcr/bcr.mk)
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
